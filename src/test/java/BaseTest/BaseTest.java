c64617ed3a81abfde9073eea7c28878b2a00eca0
Bengahero <64478533+Bengahero@users.noreply.github.com>
2021-09-02 16:40:03 +0200

    second commit

--- src/test/java/BaseTest/BaseTest.java
+++ src/test/java/BaseTest/BaseTest.java
@@ -12,16 +12,19 @@ public class BaseTest {
     public WebDriver driver;
     public HomePage homePage;
 
+
     @BeforeEach
     public void setUp(){
 
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
         options.addArguments("--incognito");
         options.addArguments("--disable-notifications");
         driver = new ChromeDriver(options);
         driver.get("https://www.google.co.in/");
         driver.manage().window().maximize();
         homePage = new HomePage(driver);
+
     }
 }
